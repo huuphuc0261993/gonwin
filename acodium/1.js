50de1c52faa1e59e9f660dcd16501cb881cb8399
tran phuc <huuphuc.0261993@gmail.com>
2020-04-25 23:38:47 +0700

    upload

---
+++ acodium/1.js
@@ -0,0 +1,13 @@
+// $(document).ready(function () {
+//     $('.ndmotkhoi').slideUp();  
+
+//     $('.motkhoi h3').click(function (e) { 
+//         $(this).next().slideToggle();
+      
+//     });
+// });
+$(document).ready(function () {
+    $('.motkhoi h3').click(function (e) { 
+        $(this).next().slideToggle();
+    });
+});
